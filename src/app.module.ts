bd5882c4997bf1b303a165d0878969be634179f6
bsarnii <scharnoui@gmail.com>
2025-03-26 20:58:09 +0100

    chg: using .env for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,18 @@ import { Status } from './task-management/boards/entities/status.entity';
 import { TasksModule } from './task-management/tasks/tasks.module';
 import { Task } from './task-management/tasks/entities/task.entity';
 import { Subtask } from './task-management/tasks/entities/subtask.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'kanban',
+      host: process.env.DATABASE_HOST,
+      port: +(process.env.DATABASE_PORT as string),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DBNAME,
       entities: [Board, Status, Task, Subtask],
       synchronize: true,
     }),
