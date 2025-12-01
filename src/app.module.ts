6292141820249ffdf0a2faa14a70498ab55a2772
Arnold Schiopu <scharnoui@gmail.com>
2025-12-01 16:46:37 +0100

    feat:  test user seeding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './task-management/boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Board } from './task-management/boards/entities/board.entity';
-import { Status } from './task-management/boards/entities/status.entity';
 import { TasksModule } from './task-management/tasks/tasks.module';
-import { Task } from './task-management/tasks/entities/task.entity';
-import { Subtask } from './task-management/tasks/entities/subtask.entity';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { User } from './users/entities/user.entity';
 import { MailModule } from './mail/mail.module';
 import { ThrottlerConfigModule } from './config/throttler-config/throttler-config.module';
+import { dbConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
@@ -20,14 +16,7 @@ import { ThrottlerConfigModule } from './config/throttler-config/throttler-confi
     }),
     ThrottlerConfigModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: +(process.env.DATABASE_PORT as string),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DBNAME,
-      entities: [Board, Status, Task, Subtask, User],
-      synchronize: true,
+      ...dbConfig
     }),
     AuthModule,
     BoardsModule,
