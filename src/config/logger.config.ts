051180dc5dd479e5bf20c07ff2cf555e4e0244af
bsarnii <scharnoui@gmail.com>
2026-09-24 21:45:20 +0200

    add: winston logger

---
+++ src/config/logger.config.ts
@@ -0,0 +1,21 @@
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+
+export const loggerConfig = WinstonModule.createLogger({
+  transports: [
+    new winston.transports.Console(),
+
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      maxFiles: '14d',
+      zippedArchive: true,
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+  ],
+});
